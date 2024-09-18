5f6bedbcd4eb824236f049948589be00495e23e0
sumanth nagireddi <sumanthnagireddi@gmail.com>
2024-09-18 21:27:43 +0530

    keys hidden

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,36 +19,18 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { MarkdownModule, MARKED_OPTIONS, provideMarkdown } from 'ngx-markdown';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes,withComponentInputBinding()),
-    provideFirebaseApp(() =>
-      initializeApp({
-        apiKey: 'AIzaSyBmj0OCb24e7stqrxqldcoodGnIFZkP-1M',
-        authDomain: 'my-portfolio-fb7d7.firebaseapp.com',
-        projectId: 'my-portfolio-fb7d7',
-        storageBucket: 'my-portfolio-fb7d7.appspot.com',
-        messagingSenderId: '832549293439',
-        appId: '1:832549293439:web:62f969174df2e8bb509e4a',
-        measurementId: 'G-8LXJE7KEFZ',
-      })
-    ),
+    provideRouter(routes, withComponentInputBinding()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideHttpClient(),
     provideAuth(() => getAuth()),
     provideAnalytics(() => getAnalytics()),
     ScreenTrackingService,
     UserTrackingService,
-    // provideAppCheck(() => {
-    //   // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-    //   const provider =
-    //     new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
-    //   return initializeAppCheck(undefined, {
-    //     provider,
-    //     isTokenAutoRefreshEnabled: true,
-    //   });
-    // }),
     provideFirestore(() => getFirestore()),
     provideDatabase(() => getDatabase()),
     provideMarkdown(),
