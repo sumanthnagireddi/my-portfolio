1e8307e79655b938e8495733c044bc3cd225a46f
sumanth nagireddi <sumanthnagireddi@gmail.com>
2024-10-01 15:38:10 +0530

    fix

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -3,26 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css'], standalone:true
+  styleUrls: ['./hero.component.css'],
+  standalone: true,
 })
 export class HeroComponent {
-  openLink(id:any){
-    
-  }
-  currentExperience:any
-  ngOnInit(){
-    this.currentExperience=this.calculateExperience()
+  openLink(id: any) {}
+  currentExperience: any;
+  ngOnInit() {
+    this.currentExperience = this.calculateExperience();
   }
   calculateExperience(): number {
     const startDate = new Date('2021-11-22');
     const endDate = new Date();
 
-    const millisecondsDifference = endDate.getTime() - startDate.getTime();
-    const daysDifference = Math.floor(millisecondsDifference / (1000 * 60 * 60 * 24));
-    const yearsExperience = Math.floor(daysDifference / 365);
-    const monthsExperience = Math.floor((daysDifference % 365) / 30);
+    // Get the difference in years, months, and days
+    let yearsExperience = endDate.getFullYear() - startDate.getFullYear();
+    let monthsExperience = endDate.getMonth() - startDate.getMonth();
+    let daysExperience = endDate.getDate() - startDate.getDate();
 
-    const totalExperience= yearsExperience + (monthsExperience / 12);
-    return parseFloat(totalExperience.toFixed(2))
+    // Adjust for negative values
+    if (daysExperience < 0) {
+      monthsExperience--;
+      // Calculate days in the previous month
+      const previousMonth = new Date(
+        endDate.getFullYear(),
+        endDate.getMonth(),
+        0
+      );
+      daysExperience += previousMonth.getDate(); // Get the last date of the previous month
+    }
+
+    if (monthsExperience < 0) {
+      yearsExperience--;
+      monthsExperience += 12;
+    }
+
+    // Calculate total experience in years
+    const totalExperience =
+      yearsExperience + monthsExperience / 12 + daysExperience / 365;
+    return parseFloat(totalExperience.toFixed(2));
   }
 }
